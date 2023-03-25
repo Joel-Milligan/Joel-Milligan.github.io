8d5ac4c58464ef2023badfa02329f95aa6003a70
Joel Milligan <joelmilligan24@gmail.com>
2023-03-26 00:18:40 +0800

    Update skills

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -5,24 +5,49 @@ const Skills = () => (
         <h2>Skills</h2>
         <section className="double-column">
             <div className="project-column">
-                <h3>Programming Languages</h3>
+                <h3>Languages</h3>
                 <ul>
-                    <Skill>.NET / C#</Skill>
-                    <Skill>React</Skill>
+                    <Skill>.NET (C#)</Skill>
+                    <Skill>Javascript</Skill>
+                    <Skill>Typescript</Skill>
                     <Skill>Rust</Skill>
-                    <Skill>Go</Skill>
-                    <Skill>Python</Skill>
-                    <Skill>Java</Skill>
+                    <Skill>C++</Skill>
+                </ul>
+
+                <h3>Frontend Frameworks</h3>
+                <ul>
+                    <Skill>React</Skill>
+                    <Skill>Angular</Skill>
+                    <Skill>Durandal</Skill>
+                    <Skill>Knockout.js</Skill>
+                </ul>
+
+                <h3>Backend Frameworks</h3>
+                <ul>
+                    <Skill>.NET WebAPI</Skill>
+                    <Skill>.NET MVC</Skill>
+                    <Skill>Entity Framework</Skill>
                 </ul>
             </div>
 
             <div className="project-column">
-                <h3>Technologies</h3>
+                <h3>Mobile Frameworks</h3>
                 <ul>
-                    <Skill>Dynamics 365</Skill>
-                    <Skill>Power Apps</Skill>
-                    <Skill>Git</Skill>
-                    <Skill>Azure Portal</Skill>
+                    <Skill>React Native</Skill>
+                    <Skill>Ionic</Skill>
+                </ul>
+                <h3>Databases</h3>
+                <ul>
+                    <Skill>MS SQL</Skill>
+                    <Skill>RavenDB</Skill>
+                    <Skill>CosmosDB</Skill>
+                </ul>
+
+                <h3>Tools / Other</h3>
+                <ul>
+                    <Skill>Azure</Skill>
+                    <Skill>D365 and Power Apps</Skill>
+                    <Skill>Git and GitHub</Skill>
                     <Skill>Azure DevOps</Skill>
                 </ul>
             </div>
